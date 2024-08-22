315d589fb7b00eea40fbe50f96c1078d81d0a710
Juha Ala-Rantala <alarantala.juha@gmail.com>
2024-08-22 23:16:22 +0300

    Working on mobile app ui

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -12,20 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+      create: (context) => LunchAppState(),
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink, brightness: Brightness.dark),
         ),
-        home: const MyHomePage(),
+        home: const LunchAppHomePage(),
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
+class LunchAppState extends ChangeNotifier {
   var current = WordPair.random();
 
   void getNext() {
@@ -34,125 +36,250 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class Restaurant {
+  final String? jsonId;
+  final String urlId;
+  final String name;
+  final String location;
+  final String? lunchHours;
+  final String? openHours;
+  final List<String> type;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Restaurant({
+    this.jsonId,
+    required this.urlId,
+    required this.name,
+    required this.location,
+    this.lunchHours,
+    this.openHours,
+    required this.type,
+  });
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) {
+    return Restaurant(
+      jsonId: json['json_id'],
+      urlId: json['url_id'],
+      name: json['name'].trim(),
+      location: json['location'],
+      lunchHours: json['lunch_hours'],
+      openHours: json['open_hours'],
+      type: List<String>.from(json['type']), // Update JSON parsing
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int currentPageIndex = 0;
+class LunchAppHomePage extends StatefulWidget {
+  const LunchAppHomePage({super.key});
+
+  @override
+  State<LunchAppHomePage> createState() => _LunchAppHomePageState();
+}
+
+class _LunchAppHomePageState extends State<LunchAppHomePage> {
+  List<Restaurant> _allRestaurants = [];
+  List<Restaurant> _filteredRestaurants = [];
+  String _selectedFilter = 'All';
+  bool _isSearching = false;
+  final TextEditingController _searchController = TextEditingController();
+  final FocusNode _searchFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRestaurants();
+  }
+
+  Future<void> _loadRestaurants() async {
+    final String jsonString = await rootBundle.loadString('assets/sodexo_restaurants.json');
+    final List<dynamic> jsonResponse = json.decode(jsonString);
+
+    List<Restaurant> restaurants = jsonResponse.map((data) => Restaurant.fromJson(data)).toList();
+
+    // Sort restaurants alphabetically by name
+    restaurants.sort((a, b) => a.name.compareTo(b.name));
+
+    setState(() {
+      _allRestaurants = restaurants;
+      _filteredRestaurants = restaurants;
+    });
+  }
+
+  void _filterRestaurants(String type) {
+    setState(() {
+      _selectedFilter = type;
+
+      List<Restaurant> filteredList;
+      if (type == 'All') {
+        filteredList = _allRestaurants;
+      } else {
+        filteredList = _allRestaurants.where((restaurant) => restaurant.type.contains(type)).toList();
+      }
+
+      if (_searchController.text.isNotEmpty) {
+        _filteredRestaurants = filteredList
+            .where((restaurant) =>
+                restaurant.name.toLowerCase().contains(_searchController.text.toLowerCase()) ||
+                restaurant.location.toLowerCase().contains(_searchController.text.toLowerCase()))
+            .toList();
+      } else {
+        _filteredRestaurants = filteredList;
+      }
+    });
+  }
+
+  void _searchRestaurants(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredRestaurants = _allRestaurants;
+      } else {
+        _filteredRestaurants = _allRestaurants
+            .where((restaurant) =>
+                restaurant.name.toLowerCase().contains(query.toLowerCase()) ||
+                restaurant.location.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
+  void _toggleSearch() {
+    setState(() {
+      _isSearching = !_isSearching;
+      _searchController.clear();
+      _filteredRestaurants = _allRestaurants;
+      _searchFocusNode.requestFocus();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.amber,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Badge(child: Icon(Icons.notifications_sharp)),
-            label: 'Notifications',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              label: Text('2'),
-              child: Icon(Icons.messenger_sharp),
-            ),
-            label: 'Messages',
+      appBar: AppBar(
+        title: _isSearching
+            ? TextField(
+                controller: _searchController,
+                focusNode: _searchFocusNode,
+                decoration: const InputDecoration(
+                  hintText: 'Search restaurants...',
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                style: const TextStyle(color: Colors.white),
+                onChanged: _searchRestaurants,
+              )
+            : const Text('Sodexo Restaurants', style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color.fromARGB(255, 223, 0, 0),
+        systemOverlayStyle: const SystemUiOverlayStyle(statusBarColor: Color.fromARGB(132, 0, 0, 0)),
+        actions: [
+          IconButton(
+            icon: Icon(_isSearching ? Icons.close : Icons.search),
+            onPressed: _toggleSearch,
           ),
         ],
       ),
-      body: <Widget>[
-        /// Home page
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-                style: theme.textTheme.titleLarge,
+      body: Column(
+        children: [
+          if (_isSearching) ...[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _filterRestaurants('All'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _selectedFilter == 'All' ? Colors.red : const Color.fromARGB(255, 102, 60, 57),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('All'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _filterRestaurants('lunch'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _selectedFilter == 'lunch' ? Colors.red : const Color.fromARGB(255, 102, 60, 57),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Lunch'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _filterRestaurants('student'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          _selectedFilter == 'student' ? Colors.red : const Color.fromARGB(255, 102, 60, 57),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Student'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _filterRestaurants('cafe'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _selectedFilter == 'cafe' ? Colors.red : const Color.fromARGB(255, 102, 60, 57),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Cafe'),
+                  ),
+                ],
               ),
             ),
-          ),
-        ),
-
-        /// Notifications page
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 1'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 2'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-            ],
-          ),
-        ),
-
-        /// Messages page
-        ListView.builder(
-          reverse: true,
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  margin: const EdgeInsets.all(8.0),
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(8.0),
+          ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredRestaurants.length,
+              itemBuilder: (context, index) {
+                final restaurant = _filteredRestaurants[index];
+                return Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          restaurant.name,
+                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            const Icon(Icons.location_on, color: Colors.grey),
+                            const SizedBox(width: 5),
+                            Text(restaurant.location),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            const Icon(Icons.access_time, color: Colors.grey),
+                            const SizedBox(width: 5),
+                            Text('Lunch: ${restaurant.lunchHours ?? "N/A"}'),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            const Icon(Icons.access_time_filled, color: Colors.grey),
+                            const SizedBox(width: 5),
+                            Text('Open: ${restaurant.openHours ?? "N/A"}'),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            const Icon(Icons.restaurant_menu, color: Colors.grey),
+                            const SizedBox(width: 5),
+                            Expanded(
+                              child: Text('Type: ${restaurant.type.join(", ")}'), // Join multiple types with a comma
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Text(
-                    'Hello',
-                    style: theme.textTheme.bodyLarge!.copyWith(color: theme.colorScheme.onPrimary),
-                  ),
-                ),
-              );
-            }
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Hi!',
-                  style: theme.textTheme.bodyLarge!.copyWith(color: theme.colorScheme.onPrimary),
-                ),
-              ),
-            );
-          },
-        ),
-      ][currentPageIndex],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- python-scripts/sodexo-scraper.py
+++ python-scripts/sodexo-scraper.py
@@ -10,14 +10,14 @@ def get_restaurant_details(link, restaurant_type, location):
     
     name = link.text
     hours = soup.find_all('span', class_='hours')
-    open_hours = hours[0].text.strip() if len(hours) > 0 else 'N/A'
-    lunch_hours = hours[1].text.strip() if len(hours) > 1 else 'N/A'
+    open_hours = hours[0].text.strip() if len(hours) > 0 else None
+    lunch_hours = hours[1].text.strip() if len(hours) > 1 else None
     
-    if open_hours == 'N/A' or lunch_hours == 'N/A':
+    if open_hours == None or lunch_hours == None:
         office_hours = soup.find_all('div', class_='office-hours__item-slots')
         if len(office_hours) > 0:
-            open_hours = office_hours[0].text.strip() if open_hours == 'N/A' else open_hours
-            lunch_hours = office_hours[1].text.strip() if len(office_hours) > 1 and lunch_hours == 'N/A' else lunch_hours
+            open_hours = office_hours[0].text.strip() if open_hours == None else open_hours
+            lunch_hours = office_hours[1].text.strip() if len(office_hours) > 1 and lunch_hours == None else lunch_hours
     
     url_id = url.split('/')[-1]
     json_url = soup.find('a', href=lambda x: x and '/weekly_json/' in x)
@@ -28,13 +28,13 @@ def get_restaurant_details(link, restaurant_type, location):
         'url_id': url_id,
         'name': name,
         'location': location,
-        'lunch_hours': lunch_hours,
         'open_hours': open_hours,
-        'type': restaurant_type
+        'lunch_hours': lunch_hours,
+        'type': [ restaurant_type ]
     }
 
 # Funktio kaikkien ravintoloiden linkkien scrapaamiseen ja tietojen keräämiseen
-def scrape_restaurants(base_url, restaurant_type):
+def scrape_restaurants(base_url, restaurant_type, all_restaurants):
     response = requests.get(base_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
@@ -45,7 +45,17 @@ def scrape_restaurants(base_url, restaurant_type):
         print("Scraping", link.text)
         location = link.find_previous('h3').text
         restaurant_details = get_restaurant_details(link, restaurant_type, location)
-        restaurants.append(restaurant_details)
+
+        # Check if the restaurant already exists
+        existing_restaurant = next((r for r in all_restaurants if r['url_id'] == restaurant_details['url_id']), None)
+        
+        if existing_restaurant:
+            # If the restaurant exists, add the new type if it's not already there
+            if restaurant_type not in existing_restaurant['type']:
+                existing_restaurant['type'].append(restaurant_details['type'][0])
+        else:
+            # If the restaurant does not exist, add it to the list
+            all_restaurants.append(restaurant_details)
     
     return restaurants
 
@@ -54,17 +64,17 @@ lunch_restaurants_url = "https://www.sodexo.fi/lounasravintolat"
 student_restaurants_url = "https://www.sodexo.fi/opiskelijaravintolat"
 cafes_url = "https://www.sodexo.fi/kahvilat"
 
+# Alusta tyhjä lista kaikkien ravintoloiden tallentamiseen
+all_restaurants = []
+
 # Scrapaa lounasravintolat
-lunch_restaurants = scrape_restaurants(lunch_restaurants_url, 'lunch')
+scrape_restaurants(lunch_restaurants_url, 'lunch', all_restaurants)
 
 # Scrapaa opiskelijaravintolat
-student_restaurants = scrape_restaurants(student_restaurants_url, 'student')
+scrape_restaurants(student_restaurants_url, 'student', all_restaurants)
 
 # Scrapaa kahvilat
-cafes = scrape_restaurants(cafes_url, 'cafe')
-
-# Yhdistä kaikki ravintolat yhteen listaan
-all_restaurants = lunch_restaurants + student_restaurants + cafes
+scrape_restaurants(cafes_url, 'cafe', all_restaurants)
 
 # Tallenna kaikki ravintolat JSON-tiedostoksi
 with open('sodexo_restaurants.json', 'w', encoding='utf-8') as json_file:
